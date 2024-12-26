899fc74b4569605f333a41fefca5b3e2208392ce
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-12-26 22:41:31 +0530

    downgrade

--- app/frontend/entrypoints/main.tsx
+++ app/frontend/entrypoints/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import App from "../pages/Example"
 
-const App = () => <div className="text-2xl text-red-500">From React!!!</div>
+const App = () => <div className="text-2xl text-red-500">From React!!! 123</div>
 
 const container = document.getElementById('root')
 console.log("root", container)
